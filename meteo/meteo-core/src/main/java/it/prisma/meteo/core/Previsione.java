16dca283e5d02f6349a58b0d351f69926474403e
aniso <aniso@patlaptop.mshome.net>
2023-05-26 12:03:00 +0200

    creato costruttore previsione

--- meteo/meteo-core/src/main/java/it/prisma/meteo/core/Previsione.java
+++ meteo/meteo-core/src/main/java/it/prisma/meteo/core/Previsione.java
@@ -7,6 +7,27 @@ public class Previsione {
 	private boolean allertaMeteo;
 	private CondizioneMeteo condizioneMeteo;
 
+	private Previsione(Citta citta, Temperatura temperatura, boolean allertaMeteo, CondizioneMeteo condizioneMeteo) {
+		super();
+		this.citta = citta;
+		this.temperatura = temperatura;
+		this.allertaMeteo = allertaMeteo;
+		this.condizioneMeteo = condizioneMeteo;
+	}
+
+	public static Previsione crea(Citta citta, Temperatura temperatura, CondizioneMeteo condizioneMeteo)
+			throws PrevisioneException {
+		if (citta == null || temperatura == null || condizioneMeteo == null) {
+			throw new PrevisioneException("Impossibile creare la previsione se ci sono dei dati mnacanti");
+		}
+		boolean allertaMeteo = false;
+		if (condizioneMeteo.equals(CondizioneMeteo.NEVE) || condizioneMeteo.equals(CondizioneMeteo.TEMPORALE)) {
+			allertaMeteo = true;
+		}
+
+		return new Previsione(citta, temperatura, allertaMeteo, condizioneMeteo);
+	}
+
 	public enum CondizioneMeteo {
 		SOLE, PIOGGIA, NUVOLOSO, NEVE, TEMPORALE;
 	}
---
+++ meteo/meteo-core/src/main/java/it/prisma/meteo/core/PrevisioneException.java
@@ -0,0 +1,9 @@
+package it.prisma.meteo.core;
+
+public class PrevisioneException extends Exception {
+
+	public PrevisioneException(String message) {
+		super(message);
+	}
+
+}
